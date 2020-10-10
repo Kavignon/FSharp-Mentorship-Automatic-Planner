1f7a4b9e3060f6de2742bb955fce903627ec6d7f
Kevin Avignon <kevin.avignon@hypertherm.com>
2020-10-10 15:36:41 -0400

    Added comments in entry point to not include CSV file in repository.
    Contains private information related to applicants. Should never be committed.

--- ConsoleApp1/ConsoleApp1/Program.fs
+++ ConsoleApp1/ConsoleApp1/Program.fs
@@ -202,6 +202,11 @@ let extractPeopleInformation (mentorshipDocument: MentorshipInformation) =
 
 [<EntryPoint>]
 let main argv =
+
+    // Don't forget to provide the current CSV document for the mentorship.
+    // Please leave the CSV document out of the repository. It's been excluded in the git ignore.
+    // Don 't commit the file in the repository.
+
     let peopleInformation = 
         MentorshipInformation.GetSample() 
         |> extractPeopleInformation
