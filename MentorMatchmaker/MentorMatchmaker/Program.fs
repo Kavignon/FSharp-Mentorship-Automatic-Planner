966d51f9c96d63174af16837e844fd5efcbe33c7
Kevin Avignon <kevin.avignon@hypertherm.com>
2020-10-11 23:08:28 -0400

    Dumping confirmed mentorship applications to a file.

--- MentorMatchmaker/MentorMatchmaker/Program.fs
+++ MentorMatchmaker/MentorMatchmaker/Program.fs
@@ -53,6 +53,11 @@ let main argv =
                         Error (NoMatchPossible csvDocumentPath)
                 
                     | Some mentorshipMatches ->
+                        mentorshipMatches
+                        |> List.map(fun (_, matches) -> matches)
+                        |> List.concat
+                        |> Matchmaking.dumpMatchingToFile
+
                         Ok mentorshipMatches
 
     let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some System.ConsoleColor.Red)
