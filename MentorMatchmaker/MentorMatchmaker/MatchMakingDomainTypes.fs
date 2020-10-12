09ee9f35610b204dc4478052fcd6eaefe8e568bb
Kevin Avignon <kevin.avignon@hypertherm.com>
2020-10-11 21:13:03 -0400

    Moved domain types into separate file.

---
+++ MentorMatchmaker/MentorMatchmaker/MatchMakingDomainTypes.fs
@@ -0,0 +1,54 @@
+﻿module MentorMatchmaker.DomainTypes
+
+open System
+
+open Utilities
+
+type DayAvailability = { WeekDayName: string; UtcHours: TimeSpan list }
+type CalendarSchedule = { AvailableDays: DayAvailability nel }
+type TimeRange = { UtcStartTime: TimeSpan; UtcEndTime: TimeSpan }
+type OverlapSchedule = { Weekday: string; MatchPeriods: TimeRange nel }
+
+type PersonInformation =
+    { Fullname: string
+      SlackName: string
+      EmailAddress: string
+      MentorshipSchedule: CalendarSchedule }
+
+type FSharpCategory =
+    | IntroductionToFSharp
+    | DeepDiveInFSharp
+    | ContributeToOpenSource
+    | WebDevelopment
+    | ContributeToCompiler
+    | MachineLearning
+    | UpForAnything
+
+type PopularityWeight =
+    | Common = 3
+    | Popular = 5
+    | Rare = 10
+
+type FsharpTopic =
+    { Category: FSharpCategory
+      PopularityWeight: PopularityWeight }
+
+type Mentor =
+    { MentorInformation: PersonInformation
+      AreasOfExpertise: FsharpTopic nel
+      SimultaneousMenteeCount: uint }
+
+type Mentee =
+    { MenteeInformation: PersonInformation
+      TopicsOfInterest: FsharpTopic nel }
+
+type PotentialMentorshipMatch =
+    { Mentor: Mentor
+      Mentee: Mentee
+      MatchingFsharpInterests: FsharpTopic list }
+
+type ConfirmedMentorshipApplication =
+    { Mentee: Mentee
+      Mentor: Mentor
+      FsharpTopic: FsharpTopic
+      MeetingTimes: OverlapSchedule nel }
--- MentorMatchmaker/MentorMatchmaker/MatchMakingInfrastructure.fs
+++ MentorMatchmaker/MentorMatchmaker/MatchMakingInfrastructure.fs
@@ -6,57 +6,9 @@ open FSharp.Data
 open FSharpPlus.Data
 
 open Utilities
+open DomainTypes
 
 type MentorshipInformation = CsvProvider<"mentorship_schema_file.csv">
-
-type DayAvailability = { WeekDayName: string; UtcHours: TimeSpan list }
-type CalendarSchedule = { AvailableDays: DayAvailability nel }
-type TimeRange = { UtcStartTime: TimeSpan; UtcEndTime: TimeSpan }
-type OverlapSchedule = { Weekday: string; MatchPeriods: TimeRange nel }
-
-type PersonInformation =
-    { Fullname: string
-      SlackName: string
-      EmailAddress: string
-      MentorshipSchedule: CalendarSchedule }
-
-type FSharpCategory =
-    | IntroductionToFSharp
-    | DeepDiveInFSharp
-    | ContributeToOpenSource
-    | WebDevelopment
-    | ContributeToCompiler
-    | MachineLearning
-    | UpForAnything
-
-type PopularityWeight =
-    | Common = 3
-    | Popular = 5
-    | Rare = 10
-
-type FsharpTopic =
-    { Category: FSharpCategory
-      PopularityWeight: PopularityWeight }
-
-type Mentor =
-    { MentorInformation: PersonInformation
-      AreasOfExpertise: FsharpTopic nel
-      SimultaneousMenteeCount: uint }
-
-type Mentee =
-    { MenteeInformation: PersonInformation
-      TopicsOfInterest: FsharpTopic nel }
-
-type PotentialMentorshipMatch =
-    { Mentor: Mentor
-      Mentee: Mentee
-      MatchingFsharpInterests: FsharpTopic list }
-
-type ConfirmedMentorshipApplication =
-    { Mentee: Mentee
-      Mentor: Mentor
-      FsharpTopic: FsharpTopic
-      MeetingTimes: OverlapSchedule nel }
 
 module private Impl =
     let availableLocalTimeHoursForMentorship = [9..23] |> List.map(fun x -> TimeSpan(x, 0, 0))
