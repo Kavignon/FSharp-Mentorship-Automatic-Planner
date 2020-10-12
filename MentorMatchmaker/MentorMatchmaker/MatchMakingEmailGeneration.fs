4b63fbeed038472e73229ed0c2a20d5d3ad29e4d
Kevin Avignon <kevin.avignon@hypertherm.com>
2020-10-12 14:25:31 -0400

    Implemented skeleton to generate template emails.

---
+++ MentorMatchmaker/MentorMatchmaker/MatchMakingEmailGeneration.fs
@@ -0,0 +1,83 @@
+﻿module MatchMakingEmailGeneration
+
+open MentorMatchmaker.Utilities
+open MentorMatchmaker.DomainTypes
+
+type MentorEmailTemplateInformation =
+    {   MentorFirstName: string
+        LengthOfMentorshipInWeeks: int
+        MenteeFirstName: string
+        Email: string
+        FssfSlack: string
+        FsharpTopic: FsharpTopic
+        MenteeAvailabilitiesInUtc: OverlapSchedule nel }
+
+type MenteeEmailTemplateInformation =
+    {   MenteeFirstName: string
+        MentorFirstName: string
+        LengthOfMentorshipInWeeks: int
+        }
+
+type MentorshipEmailTemplate =
+    | Mentor of MentorEmailTemplateInformation
+    | Mentee of MenteeEmailTemplateInformation
+
+type Email =
+    {   From: string
+        To: string
+        BodyOfText: string
+        ReplyTo: string
+        Subject: string }
+
+let dumpMatchingToFile (confirmedApplications: ConfirmedMentorshipApplication list) =
+    let dumpMeetingTimes (meetingTimes: OverlapSchedule nel) =
+        meetingTimes
+        |> NonEmptyList.map(fun meetingDay ->
+            let aggregatedTimes = meetingDay.MatchedAvailablePeriods |> NonEmptyList.toList |> List.fold(fun accumulatedTimes currentTime -> accumulatedTimes + $", {currentTime.UtcStartTime}") ""
+            let aggregatedTimes = aggregatedTimes.Substring(2)
+            $"\n {meetingDay.Weekday}: {aggregatedTimes}"
+        )
+        |> String.concat("\t\t\t")
+    
+    let dumpToFileApplicationData (application: ConfirmedMentorshipApplication) =
+        $"
+            Mentor: Name -> {application.Mentor.MentorInformation.Fullname} Email -> {application.Mentor.MentorInformation.EmailAddress}
+            Could have supported more students: {application.CouldMentorHandleMoreWork}
+            Max simultaneous students possible: {application.Mentor.SimultaneousMenteeCount}
+            Mentee: Name -> {application.Mentee.MenteeInformation.Fullname} Email -> {application.Mentee.MenteeInformation.EmailAddress}
+            Topic: {application.FsharpTopic.Name}
+            Possible meeting hours (in UTC): {dumpMeetingTimes application.MeetingTimes}
+        "
+
+    let fileContent = 
+        confirmedApplications 
+        |> List.map(fun application -> $"{dumpToFileApplicationData application}")
+        |> String.concat("\n")
+    
+    System.IO.File.WriteAllText("applicationDataDump.txt", fileContent)
+
+let dumpMailToLinksInHtmlFile (mentorshipMatches: ConfirmedMentorshipApplication list) =
+    let transformConfirmedMatchIntoMenteeEmailTemplate (confirmedMatch: ConfirmedMentorshipApplication) =
+        Mentee {
+        }
+
+    let transformConfirmedMatchIntoMentorEmailTemplate (confirmedMatch: ConfirmedMentorshipApplication) =
+        Mentor {
+        }
+
+    let transformMentorshipEmailTemplateIntoEmail (mentorshipEmailTemplate: MentorshipEmailTemplate) : Email =
+        {
+        }
+
+    let formatEmailIntoString (email: Email) : string =
+        ""
+
+    let fileContent =
+        mentorshipMatches
+        |> List.map(fun confirmedMatch -> [ transformConfirmedMatchIntoMenteeEmailTemplate confirmedMatch; transformConfirmedMatchIntoMentorEmailTemplate confirmedMatch])
+        |> List.concat
+        |> List.map transformMentorshipEmailTemplateIntoEmail
+        |> List.map formatEmailIntoString
+        |> String.concat("\n")
+
+    System.IO.File.WriteAllText("templateEmailToSendDump.txt", fileContent)
