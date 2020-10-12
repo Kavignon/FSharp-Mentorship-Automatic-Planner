f2b426b97706a70ff14e781c25a525b2f8a00530
Kevin Avignon <kevin.avignon@hypertherm.com>
2020-10-12 14:35:51 -0400

    Refactored code with modules and client's expectations.

--- MentorMatchmaker/MentorMatchmaker/MatchMakingEmailGeneration.fs
+++ MentorMatchmaker/MentorMatchmaker/MatchMakingEmailGeneration.fs
@@ -1,62 +1,62 @@
-﻿module MatchMakingEmailGeneration
+﻿module MentorMatchmaker.EmailGeneration
 
 open MentorMatchmaker.Utilities
 open MentorMatchmaker.DomainTypes
 
-type MentorEmailTemplateInformation =
-    {   MentorFirstName: string
-        LengthOfMentorshipInWeeks: int
-        MenteeFirstName: string
-        Email: string
-        FssfSlack: string
-        FsharpTopic: FsharpTopic
-        MenteeAvailabilitiesInUtc: OverlapSchedule nel }
+module private Implementation =
+    type MentorEmailTemplateInformation =
+        {   MentorFirstName: string
+            LengthOfMentorshipInWeeks: int
+            MenteeFirstName: string
+            Email: string
+            FssfSlack: string
+            FsharpTopic: FsharpTopic
+            MenteeAvailabilitiesInUtc: OverlapSchedule nel }
 
-type MenteeEmailTemplateInformation =
-    {   MenteeFirstName: string
-        MentorFirstName: string
-        LengthOfMentorshipInWeeks: int
-        }
+    type MenteeEmailTemplateInformation =
+        {   MenteeFirstName: string
+            MentorFirstName: string
+            LengthOfMentorshipInWeeks: int
+            }
 
-type MentorshipEmailTemplate =
-    | Mentor of MentorEmailTemplateInformation
-    | Mentee of MenteeEmailTemplateInformation
+    type MentorshipEmailTemplate =
+        | Mentor of MentorEmailTemplateInformation
+        | Mentee of MenteeEmailTemplateInformation
 
-type Email =
-    {   From: string
-        To: string
-        BodyOfText: string
-        ReplyTo: string
-        Subject: string }
+    type Email =
+        {   From: string
+            To: string
+            BodyOfText: string
+            ReplyTo: string
+            Subject: string }
 
-let dumpMatchingToFile (confirmedApplications: ConfirmedMentorshipApplication list) =
-    let dumpMeetingTimes (meetingTimes: OverlapSchedule nel) =
-        meetingTimes
-        |> NonEmptyList.map(fun meetingDay ->
-            let aggregatedTimes = meetingDay.MatchedAvailablePeriods |> NonEmptyList.toList |> List.fold(fun accumulatedTimes currentTime -> accumulatedTimes + $", {currentTime.UtcStartTime}") ""
-            let aggregatedTimes = aggregatedTimes.Substring(2)
-            $"\n {meetingDay.Weekday}: {aggregatedTimes}"
-        )
-        |> String.concat("\t\t\t")
+    let dumpMatchingToFile (confirmedApplications: ConfirmedMentorshipApplication list) =
+        let dumpMeetingTimes (meetingTimes: OverlapSchedule nel) =
+            meetingTimes
+            |> NonEmptyList.map(fun meetingDay ->
+                let aggregatedTimes = meetingDay.MatchedAvailablePeriods |> NonEmptyList.toList |> List.fold(fun accumulatedTimes currentTime -> accumulatedTimes + $", {currentTime.UtcStartTime}") ""
+                let aggregatedTimes = aggregatedTimes.Substring(2)
+                $"\n {meetingDay.Weekday}: {aggregatedTimes}"
+            )
+            |> String.concat("\t\t\t")
     
-    let dumpToFileApplicationData (application: ConfirmedMentorshipApplication) =
-        $"
-            Mentor: Name -> {application.Mentor.MentorInformation.Fullname} Email -> {application.Mentor.MentorInformation.EmailAddress}
-            Could have supported more students: {application.CouldMentorHandleMoreWork}
-            Max simultaneous students possible: {application.Mentor.SimultaneousMenteeCount}
-            Mentee: Name -> {application.Mentee.MenteeInformation.Fullname} Email -> {application.Mentee.MenteeInformation.EmailAddress}
-            Topic: {application.FsharpTopic.Name}
-            Possible meeting hours (in UTC): {dumpMeetingTimes application.MeetingTimes}
-        "
+        let dumpToFileApplicationData (application: ConfirmedMentorshipApplication) =
+            $"
+                Mentor: Name -> {application.Mentor.MentorInformation.Fullname} Email -> {application.Mentor.MentorInformation.EmailAddress}
+                Could have supported more students: {application.CouldMentorHandleMoreWork}
+                Max simultaneous students possible: {application.Mentor.SimultaneousMenteeCount}
+                Mentee: Name -> {application.Mentee.MenteeInformation.Fullname} Email -> {application.Mentee.MenteeInformation.EmailAddress}
+                Topic: {application.FsharpTopic.Name}
+                Possible meeting hours (in UTC): {dumpMeetingTimes application.MeetingTimes}
+            "
 
-    let fileContent = 
-        confirmedApplications 
-        |> List.map(fun application -> $"{dumpToFileApplicationData application}")
-        |> String.concat("\n")
+        let fileContent = 
+            confirmedApplications 
+            |> List.map(fun application -> $"{dumpToFileApplicationData application}")
+            |> String.concat("\n")
     
-    System.IO.File.WriteAllText("applicationDataDump.txt", fileContent)
+        System.IO.File.WriteAllText("applicationDataDump.txt", fileContent)
 
-let dumpMailToLinksInHtmlFile (mentorshipMatches: ConfirmedMentorshipApplication list) =
     let transformConfirmedMatchIntoMenteeEmailTemplate (confirmedMatch: ConfirmedMentorshipApplication) =
         Mentee {
         }
@@ -72,12 +72,15 @@ let dumpMailToLinksInHtmlFile (mentorshipMatches: ConfirmedMentorshipApplication
     let formatEmailIntoString (email: Email) : string =
         ""
 
-    let fileContent =
-        mentorshipMatches
-        |> List.map(fun confirmedMatch -> [ transformConfirmedMatchIntoMenteeEmailTemplate confirmedMatch; transformConfirmedMatchIntoMentorEmailTemplate confirmedMatch])
-        |> List.concat
-        |> List.map transformMentorshipEmailTemplateIntoEmail
-        |> List.map formatEmailIntoString
-        |> String.concat("\n")
+[<RequireQualifiedAccess>]
+module EmailGenerationService =
+    open Implementation
 
-    System.IO.File.WriteAllText("templateEmailToSendDump.txt", fileContent)
+    let dumpTemplateEmailsInFile (mentorshipMatch: ConfirmedMentorshipApplication) =
+        let fileContent =
+            [ transformConfirmedMatchIntoMenteeEmailTemplate mentorshipMatch; transformConfirmedMatchIntoMentorEmailTemplate mentorshipMatch]
+            |> List.map transformMentorshipEmailTemplateIntoEmail
+            |> List.map formatEmailIntoString
+            |> String.concat("\n")
+
+        System.IO.File.WriteAllText("templateEmailToSendDump.txt", fileContent)
